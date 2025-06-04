4dcfc8fbac5586aca7365d4844d71e4290cdd3f3
SVS <vishnus.jt@gmail.com>
2025-06-04 07:54:11 +0530

    initialmessage

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
     public static void main(String[] args){
 
         System.out.println("this is git");
+        System.out.println("final");
 
         System.out.println("this is hub");
         Scanner sc=new Scanner(System.in);
