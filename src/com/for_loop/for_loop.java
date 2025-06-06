ef678efb2cb9d43554ee105f2b65a826f913aa85
SVS <vishnus.jt@gmail.com>
2025-06-06 07:27:51 +0530

    for-loop

---
+++ src/com/for_loop/for_loop.java
@@ -0,0 +1,15 @@
+package com.for_loop;
+
+public class for_loop {
+    public static void main(String[] args){
+        for(int i=5;i<=20;i++){
+            System.out.println(i);
+        }
+        System.out.println("ascending order completed");
+        for(int j=20;j>=5;j--){
+            System.out.println(j);
+        }
+        System.out.println("descending order completed");
+    }
+
+}
